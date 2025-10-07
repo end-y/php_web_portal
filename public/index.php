6599409cc20f894355180a8dc11e398ce674d5d5
end-y <enderyazici@yandex.com>
2025-10-07 14:07:50 +0300

    revert

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-/*
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/components/TaskColorList.php';
 require_once __DIR__ . '/../src/components/TaskOrderList.php';
@@ -22,12 +21,51 @@ if (isset($_GET['partial']) && $_GET['partial'] === '1') {
     exit;
 }
 ob_start();
-*/
+
 ?>
 
-<?php
-echo "Hello World";
-?>
+<div class="bg-white rounded-lg shadow-md p-6">
+
+    <!-- Modal butonu (tablonun dışında) -->
+    <?php echo renderTaskModal(); ?>
+
+
+    <!-- Arama Kutusu -->
+    <form class="mb-4 flex items-center gap-2">
+        <input
+            name="search"
+            value="<?php echo Utils::e($_GET['search'] ?? ''); ?>"
+            type="text"
+            id="searchInput"
+            placeholder="Search tasks..."
+            class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+        <input type="button" class="bg-red-500 cursor-pointer text-white px-4 py-2 rounded-md" value="Clear filters" onclick="window.location.href = '/';">
+    </form>
+
+    <!-- Görev Tablosu -->
+    <div class="overflow-x-auto">
+        <table class="w-full table-auto border-collapse">
+            <thead>
+                <tr class="bg-gray-100">
+                    <?php echo renderTaskColorList($colorTasks); ?>
+                    <?php echo renderTaskOrderList("title", "Task"); ?>
+                    <?php echo renderTaskOrderList("description", "Title"); ?>
+                </tr>
+            </thead>
+            <tbody id="taskTable">
+                <?php foreach($tasks as $task): ?>
+                    <?php $colorClass = TaskController::getColorClass(Utils::e($task["colorCode"])); ?>
+                    <tr class="border-b hover:bg-gray-50 transition">
+                        <td class="px-4 <?php echo $colorClass; ?> py-3"><?php echo Utils::e($task["task"]); ?></td>
+                        <td class="px-4 <?php echo $colorClass; ?> py-3"><?php echo Utils::e($task["title"]); ?></td>
+                        <td class="px-4 <?php echo $colorClass; ?> py-3"><?php echo Utils::e($task["description"]); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <?php
 $content = ob_get_clean();
